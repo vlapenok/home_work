15f093bb3d6deb870aaf638ba9bcf1f19e197e6a
Vadim Lapenok <vazzim@narod.ru>
2020-12-22 17:30:43 +0300

    Закончил задание номер 6

--- src/home_work/lesson_6/Main.java
+++ src/home_work/lesson_6/Main.java
@@ -5,31 +5,33 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random rnd = new Random();
-        Cat cat = new Cat("Барсик", 200, 2, 7);
-        Dog dog = new Dog("Тузик", 500, 15, 2);
+        Cat bars = new Cat("Барсик", 300, 2, 10);
+        Cat kuz = new Cat("Кузя", 200, 2, 7);
+        Dog dina = new Dog("Дина", 500, 15, 2);
+        Dog villi = new Dog("Вилли", 250, 7, 1);
 
-        Animal[] animals = {cat, dog};
+        Animal[] animals = {bars, kuz, dina, villi};
         int toRun = rnd.nextInt(600);
         int toSwim = rnd.nextInt(30);
-        int toJump = rnd.nextInt(10);
+        int toJump = rnd.nextInt(15);
 
         for (Animal animal : animals) {
 
             String canRun = animal.run(toRun) ? "добежит" : "не добежит";
             String animalMaxRun = animal.getType() + " " + animal.getName() + " бежит максимум " + animal.getMaxRun() + " метров, ";
-            String run = "если " + animal.getName() + "у назначить расстояние в " + toRun + " метров, то он " + canRun + ".";
+            String run = "если " + animal.getName() + " назначить расстояние в " + toRun + " метров, то он " + canRun + ".";
             System.out.println(animalMaxRun + run);
 
             String canJump = animal.jump(toJump) ? "допрыгнет" : "не допрыгнет";
             String animalMaxJump = animal.getType() + " " + animal.getName() + " прыгает на " + animal.getMaxJump() + " метра, ";
-            String jump = "если " + animal.getName() + "у дать высоту в " + toJump + " метра, то он " + canJump + ".";
+            String jump = "если " + animal.getName() + " дать высоту в " + toJump + " метра, то он " + canJump + ".";
             System.out.println(animalMaxJump + jump);
 
             int checkSwim = animal.swim(toSwim);
             String canSwim = checkSwim == animal.swimOk ? "доплывет" : "не доплывет";
             if(checkSwim == animal.dontSwim) canSwim = "не полезет в воду";
             String animalMaxSwim = animal.getType() + " " + animal.getName() + " плавает на " + animal.getMaxSwim() + " метров, ";
-            String swim = "если " + animal.getName() + "у назначить расстояние в " + toSwim + " метров, то он " + canSwim + ".";
+            String swim = "если " + animal.getName() + " назначить расстояние в " + toSwim + " метров, то он " + canSwim + ".";
             System.out.println(animalMaxSwim + swim);
 
             System.out.println();
